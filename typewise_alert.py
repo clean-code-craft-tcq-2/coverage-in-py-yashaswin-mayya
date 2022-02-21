9f222d615a6f6a7aa1a9eadc57b728f428285e8c
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-02-21 11:58:20 +0530

    Initial Commit

--- typewise_alert.py
+++ typewise_alert.py
@@ -10,15 +10,14 @@ def infer_breach(value, lowerLimit, upperLimit):
 def classify_temperature_breach(coolingType, temperatureInC):
   lowerLimit = 0
   upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+
+  reference_for_upperLimit = {
+    'PASSIVE COOLING' : 35,
+    'HI_ACTIVE_COOLING' : 45,
+    'MED_ACTIVE_COOLING' : 40
+  }
+  upperLimit = reference_for_upperLimit[coolingType]
+
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
 
